6550993357e591a467c4243fe3161a766dd0ca90
Sotaneum <gnyontu39@gmail.com>
2022-08-27 11:50:03 +0900

    [refactoring] addon 호환성 수정

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -5,8 +5,7 @@ import { TooltipLinkList } from "@storybook/components";
 
 import { DEFAULT_USER_AGENT_PARAMETER } from "../constants";
 
-import { TooltipProps, UserAgentParameter } from "../types";
-import { Link } from "@storybook/components/dist/ts3.9";
+import { Link, TooltipProps, UserAgentParameter } from "../types";
 
 export default function ({ onChange }: TooltipProps) {
   const [args, updateArgs] = useArgs();
