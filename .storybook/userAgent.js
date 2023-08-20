3e828cad4c2ba1722aefb08c03b0e7024ecbc8c9
Sotaneum <gnyontu39@gmail.com>
2023-08-21 00:45:23 +0900

    [v6.3.0] 커스텀 목록 예제 수정

--- .storybook/userAgent.js
+++ .storybook/userAgent.js
@@ -1,7 +1,7 @@
-// export const customUserAgents = [
-//   {
-//     name: "Windows_7-IE_11",
-//     userAgent:
-//       "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
-//   },
-// ];
+export const customUserAgents = [
+  {
+    name: "Windows_7-IE_11",
+    userAgent:
+      "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+  },
+];
