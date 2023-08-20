4da72b54d17ba7d9d677543e735e22cdf38efa89
Sotaneum <gnyontu39@gmail.com>
2023-08-20 17:43:08 +0900

    [v6.3.0] toUserAgentDetail 삭제

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -19,34 +19,3 @@ export function changeUserAgent(userAgent: string, iframe: HTMLIFrameElement) {
     configurable: true,
   });
 }
-
-export function toUserAgentDetail(userAgent: string): string[] {
-  if (!userAgent) {
-    return [];
-  }
-  let prev = "";
-  return userAgent
-    .split(/(\s?\(?[\w\/.,:;\s]+\)?\s?)/g)
-    .reduce(
-      (items, item) =>
-        items.concat(
-          ...(item.slice(0, 1) === "("
-            ? [item.slice(1, item.length - 2)]
-            : item.split(" "))
-        ),
-      []
-    )
-    .filter((item) => !!item)
-    .reduce((items, item) => {
-      if (item === "like") {
-        prev = "like";
-        return items;
-      }
-      if (prev === "") {
-        return items.concat(item);
-      }
-      const newItem = `${prev} ${item}`;
-      prev = "";
-      return items.concat(newItem);
-    }, []);
-}
