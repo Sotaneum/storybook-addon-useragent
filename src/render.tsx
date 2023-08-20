d697cb7a9c3d496db0ec04e7985505ef510f8942
Sotaneum <gnyontu39@gmail.com>
2023-08-20 17:54:23 +0900

    [v6.3.0] force re-render 제거

--- src/render.tsx
+++ src/render.tsx
@@ -6,17 +6,15 @@ export default makeDecorator({
   wrapper: (storyFn, context) => {
     const nowUserAgent: string = context.globals.activeUserAgent ?? "";
 
-    const [key, setKey] = useState(Date.now());
     const [userAgent, setUserAgent] = useState(nowUserAgent);
 
     useEffect(() => {
       const nextUserAgent = JSON.stringify(nowUserAgent);
       if (userAgent !== nextUserAgent) {
-        setKey(Date.now());
         setUserAgent(nextUserAgent);
       }
     }, [nowUserAgent]);
 
-    return storyFn({ ...context, key });
+    return storyFn(context);
   },
 });
