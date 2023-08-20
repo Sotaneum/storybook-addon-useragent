08666cb0ec42ec59cd16de3b79613f031e06fd91
Sotaneum <gnyontu39@gmail.com>
2023-08-21 01:09:27 +0900

    [v6.3.0] params key 통일

--- src/render.tsx
+++ src/render.tsx
@@ -1,10 +1,11 @@
 import { useEffect, makeDecorator } from "@storybook/addons";
 import setUserAgent from "./core";
 import { getUserAgent } from "./utils";
+import { PARAM_KEY } from "./constants";
 
 export default makeDecorator({
   name: "userAgentRender",
-  parameterName: "userAgent",
+  parameterName: PARAM_KEY,
   wrapper: (storyFn, context) => {
     const userAgent = getUserAgent(context.args);
 
