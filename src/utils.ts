e4e45dbeeada8d42ac69c129efe852bff2b0fdea
LEE DONG GUN <gnyontu39@gmail.com>
2023-08-25 21:34:48 +0900

    [feature/remove-backward-compatibility] 이전 호환성 제거 (#17)

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,3 @@
 export function getUserAgent(args?: { [key: string]: string }) {
-  if (!args) {
-    return "";
-  }
-  if (args["useragent"]) {
-    return args["useragent"];
-  }
-  if (args["activeUserAgent"]) {
-    console.warn(
-      '"activeUserAgent" is an argument that will be removed in v6.4.0. Use "useragent".'
-    );
-    return args["activeUserAgent"];
-  }
-  return "";
+  return !args || !args["useragent"] ? args["useragent"] : "";
 }
