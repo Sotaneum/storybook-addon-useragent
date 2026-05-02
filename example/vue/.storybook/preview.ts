8e7c36671ba10b48652caff299becb169b46464e
sotaneum <gnyontu39@gmail.com>
2026-05-02 20:53:49 +0900

    chore: 예제 업데이트

--- example/vue/.storybook/preview.ts
+++ example/vue/.storybook/preview.ts
@@ -1,3 +1,10 @@
+import type { Preview } from "@storybook/vue3";
 import { customUserAgents } from "./userAgent";
 
-export const parameters = { userAgent: customUserAgents };
+const preview: Preview = {
+  parameters: {
+    userAgent: customUserAgents,
+  },
+};
+
+export default preview;
