50e865028f76006c17b927c2d7d77ec9eaa97665
sotaneum <gnyontu39@gmail.com>
2026-05-02 21:45:19 +0900

    feat: 예제 개선

--- example/react19/.storybook/main.ts
+++ example/react19/.storybook/main.ts
@@ -1,17 +1,12 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-docs",
-    "storybook-addon-useragent"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-docs", "storybook-addon-useragent"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
 };
+
 export default config;
